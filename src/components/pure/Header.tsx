a59b71f8116b1c6ae3e21ebd8dfff30704798db0
Federico-Baez <baezfede95@gmail.com>
2023-12-31 00:12:45 -0300

    first header stage finished

--- src/components/pure/Header.tsx
+++ src/components/pure/Header.tsx
@@ -3,26 +3,50 @@ import { DarkThemeIcon, LightThemeIcon, MenuIcon, ScriptIcon } from "./icons";
 export default function Header() {
 	return (
 		<header className="bg-white px-6 shadow">
-			<div className="flex h-16 items-center justify-between">
-				<button className="-m-1 rounded p-1 text-stone-500 transition-colors hover:bg-stone-200 hover:text-violet-400 focus:ring-2 focus:ring-stone-500">
+			<div className="mx-auto flex h-16 max-w-6xl items-center justify-between">
+				<button className="-m-1 rounded p-1 text-stone-500 transition-colors hover:bg-stone-200 hover:text-violet-600 focus:ring-2 focus:ring-stone-500">
 					<MenuIcon />
 				</button>
 				{/* logo + nav desktop */}
-				<div>
+				<div className="flex items-center">
 					<a
 						href="#"
-						className="text-violet-400 duration-200 hover:rotate-6"
+						className="text-violet-600 duration-200 hover:rotate-6"
 					>
 						<ScriptIcon />
 					</a>
+					{/* nav desktop */}
+					<div className="ml-8 hidden space-x-8 md:flex">
+						<a className="px-3 py-2  text-violet-600" href="#">
+							Home
+						</a>
+						<a
+							className="px-3 py-2 text-stone-900 transition-colors hover:text-violet-600"
+							href="#"
+						>
+							About
+						</a>
+						<a
+							className="px-3 py-2 text-stone-900 transition-colors hover:text-violet-600"
+							href="#"
+						>
+							Blog
+						</a>
+						<a
+							className="px-3 py-2 text-stone-900 transition-colors hover:text-violet-600"
+							href="#"
+						>
+							Contact
+						</a>
+					</div>
 				</div>
 
 				<div className="flex items-center gap-4">
-					<button className="rounded-full text-stone-500 transition-colors hover:bg-stone-200 hover:text-violet-400 focus:ring-2 focus:ring-stone-500">
+					<button className="rounded-full text-stone-500 transition-colors hover:bg-stone-200 hover:text-violet-600 focus:ring-2 focus:ring-stone-500">
 						<LightThemeIcon />
 						{/* <DarkThemeIcon /> */}
 					</button>
-					<button className="h-8 w-8 rounded-full text-stone-500 transition-colors hover:bg-stone-200 hover:text-violet-400 focus:ring-2 focus:ring-stone-500">
+					<button className="h-8 w-8 rounded-full text-stone-500 transition-colors hover:bg-stone-200 hover:text-violet-600 focus:ring-2 focus:ring-stone-500">
 						ES
 					</button>
 				</div>
@@ -31,25 +55,25 @@ export default function Header() {
 			<div className="space-y-1 border-t py-2 md:hidden">
 				<a
 					href="#"
-					className="block rounded-md bg-violet-400 px-3 py-2 text-white"
+					className="block rounded-md bg-violet-600 px-3 py-2 text-white"
 				>
 					Home
 				</a>
 				<a
 					href="#"
-					className="block rounded-md px-3 py-2 transition-colors hover:bg-violet-400 hover:text-white"
+					className="block rounded-md px-3 py-2 text-stone-900 transition-colors hover:bg-violet-600 hover:text-white"
 				>
 					About
 				</a>
 				<a
 					href="#"
-					className="block rounded-md px-3 py-2 transition-colors hover:bg-violet-400 hover:text-white"
+					className="block rounded-md px-3 py-2 text-stone-900 transition-colors hover:bg-violet-600 hover:text-white"
 				>
 					Blog
 				</a>
 				<a
 					href="#"
-					className="block rounded-md px-3 py-2 transition-colors hover:bg-violet-400 hover:text-white"
+					className="block rounded-md px-3 py-2 text-stone-900 transition-colors hover:bg-violet-600 hover:text-white"
 				>
 					Contact
 				</a>
